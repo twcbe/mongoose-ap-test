d620d5c87c91810413a3b977071c1c80b8fcb3f6
Abdul Khader Jeelani <abdulkj@thoughtworks.com>
2018-11-12 20:55:21 +0530

    http test

--- src/main.c
+++ src/main.c
@@ -1,23 +1,44 @@
 #include "mgos.h"
 #include "mgos_wifi.h"
 
-const struct mgos_config_wifi_ap config;
+int counter = 0;
+
+static void http_cb(struct mg_connection *c, int ev, void *ev_data, void *ud)
+{
+
+  switch (ev)
+  {
+  case MG_EV_CONNECT:
+    break;
+  case MG_EV_HTTP_CHUNK:
+  {
+
+    LOG(LL_INFO, ("Finished fetching "));
+    c->flags |= MG_F_DELETE_CHUNK;
+    break;
+  }
+  case MG_EV_HTTP_REPLY:
+
+    LOG(LL_INFO, ("Finished fetching"));
+    c->flags |= MG_F_CLOSE_IMMEDIATELY;
+    break;
+  case MG_EV_CLOSE:
+    LOG(LL_INFO, ("status"));
+    break;
+  }
+}
+
+static void timer_cb(void *arg)
+{
+  LOG(LL_INFO, ("counter: %d", counter++));
+  (void)arg;
+}
 
 enum mgos_app_init_result mgos_app_init(void)
 {
-  // config.enable = 1;
-  // config.ssid = "Incubator__??????";
-  // config.pass = "12345678";
-  // config.hidden = 0;
-  // config.channel = 6;
-  // config.max_connections = 2;
-  // config.ip = "192.168.4.1";         // Static IP Address
-  // config.netmask = "255.255.255.0";  // Static Netmas
-  // config.gw = "192.168.4.1";         // Static Default Gatway
-  // config.dhcp_start = "192.168.4.2"; // DHCP Start Addres
-  // config.dhcp_end = "192.168.4.100"; // DHCP End Addres
-  // config.trigger_on_gpio = -1;       // Trigger AP on low GPI
 
-  mgos_wifi_setup_ap(&config);
+  mgos_set_timer(35000, MGOS_TIMER_REPEAT, timer_cb, NULL);
+  mg_connect_http(mgos_get_mgr(), http_cb, NULL, "http://www.google.com", NULL, NULL);
+  // mgos_set_wifi_ap_enable(&mgos_sys_config, false);
   return MGOS_APP_INIT_SUCCESS;
 }
