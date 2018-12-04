e3a6f96d15ab68a65802067d64fe82a5c818d095
Abdul Khader Jeelani <abdulkj@thoughtworks.com>
2018-12-04 15:02:27 +0530

    [Abdul] Show wifi ap name

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "mgos.h"
 #include "mgos_wifi.h"
+#include "mgos_rpc.h"
+#include "mgos_ro_vars.h"
 #include "mgos_wifi_captive_portal.h"
 #define LAMBDA(c_) ({ c_ _; })
 int counter = 0;
@@ -39,6 +41,10 @@ static void timer_cb(void *arg)
        mgos_uptime(), (unsigned long)mgos_get_heap_size(),
        (unsigned long)mgos_get_free_heap_size()));
   s_tick_tock = !s_tick_tock;
+
+  const char *ssid = mgos_config_get_wifi_ap_ssid(&mgos_sys_config);
+  LOG(LL_INFO, ("Wifi ssid is %s", ssid));
+
   (void)arg;
 }
 
@@ -76,16 +82,16 @@ static void wifi_cb(int ev, void *evd, void *arg)
     // mgos_config_set_wifi_ap_enable(&mgos_sys_config, 1);
     // mgos_usleep(500);
     // mgos_system_restart();
-    disconnects++;
-    if (disconnects >= 5)
-    {
-      LOG(LL_INFO, ("WiFi STA disconnected %d times, Starting portal", disconnects));
-      mgos_sys_config_set_wifi_sta_enable(false);
-      mgos_sys_config_set_wifi_ap_enable(true);
-      mgos_wifi_captive_portal_start();
-      mgos_system_restart();
-      disconnects = 0;
-    }
+    // disconnects++;
+    // if (disconnects >= 5)
+    // {
+    //   LOG(LL_INFO, ("WiFi STA disconnected %d times, Starting portal", disconnects));
+    // mgos_sys_config_set_wifi_sta_enable(false);
+    mgos_sys_config_set_wifi_ap_enable(true);
+    mgos_wifi_captive_portal_start();
+    // mgos_system_restart();
+    // disconnects = 0;
+    // }
     break;
   case MGOS_WIFI_EV_STA_CONNECTING:
     LOG(LL_INFO, ("WiFi STA connecting %p", arg));
@@ -104,12 +110,6 @@ static void wifi_cb(int ev, void *evd, void *arg)
                   aa->mac[0], aa->mac[1], aa->mac[2], aa->mac[3], aa->mac[4],
                   aa->mac[5]));
 
-    LOG(LL_INFO, ("WiFi AP STA connected MAC %d %d %d %d %d %d",
-                  aa->mac[0], aa->mac[1], aa->mac[2], aa->mac[3], aa->mac[4],
-                  aa->mac[5]));
-
-    const char *ssid = mgos_config_get_wifi_ap_ssid(&mgos_sys_config);
-    LOG(LL_INFO, ("Wifi ssid is %s", ssid));
     break;
   }
   case MGOS_WIFI_EV_AP_STA_DISCONNECTED:
@@ -140,25 +140,11 @@ static int_identity get_id_function()
   return id;
 }
 
-enum mgos_app_init_result
-mgos_app_init(void)
+enum mgos_app_init_result mgos_app_init(void)
 {
   mgos_set_timer(35000, MGOS_TIMER_REPEAT, timer_cb, NULL);
   mgos_event_add_group_handler(MGOS_WIFI_EV_BASE, wifi_cb, NULL);
   mgos_event_add_group_handler(MGOS_EVENT_GRP_NET, net_cb, NULL);
-
-  // uint8_t mac[6];
-  // esp_wifi_get_mac(ESP_IF_WIFI_AP, mac);
-  // LOG(LL_ERROR, ("Local MAC Address: %.2x:%.2x:%.2x:%.2x:%.2x:%.2x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]));
-
-  // // char mac_str[6];
-  // uint8_t mac_uin8t[6];
-  // device_get_mac_address(mac_uin8t);
-  // int i;
-  // for (i = 0; i < 6; ++i)
-  // {
-  //   LOG(LL_INFO, ("%d", mac_uin8t[i]));
-  // } // sprintf(mac_str, "%02x%02x%02x%02x%02x%02x\n", mac_uin8t[0] & 0xFF, mac_uin8t[1] & 0xFF, mac_uin8t[2] & 0xFF, mac_uin8t[3] & 0xFF, mac_uin8t[4] & 0xFF, mac_uin8t[5] & 0xFF);
 
   int_identity int_id = LAMBDA(int _(int v) {
     return v;
@@ -168,5 +154,14 @@ mgos_app_init(void)
     return v;
   }));
   LOG(LL_INFO, ("%d", result));
+
+  const char *ssid = mgos_config_get_wifi_ap_ssid(&mgos_sys_config);
+  LOG(LL_INFO, ("Wifi ssid is %s", ssid));
+
+  struct mg_str s = mg_mk_str(ssid);
+  struct mg_str mac = mg_mk_str(mgos_sys_ro_vars_get_mac_address());
+  mgos_expand_placeholders(mac, &s);
+
+  LOG(LL_INFO, ("Wifi ssid with mac %s", s.p));
   return MGOS_APP_INIT_SUCCESS;
 }
